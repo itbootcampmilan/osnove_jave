85cf5580c049d1ed0b151123674eb0bd02d3956a
Milan Jovanovic <it.bootcamp.drive@gmail.com>
2022-05-13 21:04:44 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,11 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add putanja_do_fajla");
+        System.out.println("git commit -m poruka ");
+        System.out.println("git push");
+    }
+}
